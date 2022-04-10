2e4b446c678f22d005cbcd608257c147459150c7
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-10 23:25:13 +0500

    fix(BtnSwitchTheme): сделать кнопку рабочей

--- src/components/BtnSwitchTheme/BtnSwitchTheme.js
+++ src/components/BtnSwitchTheme/BtnSwitchTheme.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import './BtnSwitchTheme.css';
 
 function BtnSwitchTheme () {
+  const [theme, setTheme] = useState(localStorage.getItem('app-theme') || 'dark');
+  
+  const handleThemeClick = () => {
+    const btnThemeChecker = document.querySelector('.btn-switch-theme__checker');
+    btnThemeChecker.checked ? setTheme('light') : setTheme('dark');
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('app-theme', theme);
+  }, [theme]);
+
   return (
-    <label className='btn-switch-theme'>
-      <input type='checkbox' className='btn-switch-theme__checker' />
-      <span className='btn-switch-theme__slider' />
+    <label className='btn-switch-theme' >
+      <input 
+        type='checkbox' 
+        defaultChecked={localStorage.getItem('app-theme') === 'light'} 
+        onClick={handleThemeClick} 
+        className='btn-switch-theme__checker' 
+      />
+      <span className='btn-switch-theme__image' />
     </label>
   );
 }
