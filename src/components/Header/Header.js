7bc3590edcdad3a1dfcaa1b9df0448d849032477
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-10 15:55:51 +0500

    feat(Header): верстка хедера

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import './Header.css';
 import React from 'react';
+import logo from '../../images/logo.svg';
+import BtnSwitchTheme from '../BtnSwitchTheme/BtnSwitchTheme';
 
 function Header () {
   return (
-    <header className="header">
-      <h1>Header</h1>
+    <header className='header'>
+      <img className='header__logo' src={logo} />
+      <BtnSwitchTheme />
     </header>
   );
 }
