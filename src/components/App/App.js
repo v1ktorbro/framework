a962312d14930483dc2fa6eaecd6db1fd504a30b
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-11 23:36:19 +0500

    improve(App): стейт темы создается в App, начальная тема подгружается из ОС

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import Header from '../Header/Header';
 import NavSearch from '../NavSearch/NavSearch';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // цвет темы подтягивается из настроек ОС и сохраняется в localStorage
+  const isNightTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches;
+  const defaultTheme = isNightTheme ? 'night' : 'day';
+  const [theme, setTheme] = useState(localStorage.getItem('app-theme') || defaultTheme);
+
+  useEffect(() => {
+    localStorage.setItem('app-theme', theme);
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header 
+        theme={theme}
+        setTheme={setTheme}
+      />
       <NavSearch />
     </>
   );
