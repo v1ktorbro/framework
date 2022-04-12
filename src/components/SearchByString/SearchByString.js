3d6f001a6d9db2ba0a88aa0b4653cd41ed59871b
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-12 19:42:18 +0500

    improve(SearchByString): подключение компонента DropDownList

--- src/components/SearchByString/SearchByString.js
+++ src/components/SearchByString/SearchByString.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './SearchByString.css';
 import BtnResetCross from '../BtnResetCross/BtnResetCross';
+import DropDownList from '../DropDownList/DropDownList';
 
-function SearchByString({ placeholder, theme }) {
+function SearchByString({ placeholder, theme, data }) {
   const [searchString, setSearchString] = useState('');
 
   const onChangeSearch = (evt) => {
@@ -15,22 +16,28 @@ function SearchByString({ placeholder, theme }) {
   }
 
   return (
-    <div className={`search-by-string search-by-string_${theme}`}>
-      <input 
-        className={`search-by-string__input search-by-string__input_${theme}`}
-        type='text'
-        value={searchString}
-        onChange={onChangeSearch}
-        placeholder={placeholder}
-      />
-      { searchString.length > 0 &&
+    <>
+      <div className={`search-by-string search-by-string_${theme}`}>
+        <input 
+          className={`search-by-string__input search-by-string__input_${theme}`}
+          type='text'
+          value={searchString}
+          onChange={onChangeSearch}
+          placeholder={placeholder}
+        />
+        { searchString.length > 0 &&
           <BtnResetCross 
             hStyle={{right: '18px'}}
             handleReset={handleResetButton}
             theme={theme}
           />
-      }
-    </div>
+        }
+        <DropDownList
+          theme={theme}
+          data={data}
+        />
+      </div>
+    </>
   );
 }
 
