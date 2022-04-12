33cabf5d8cd61bc7e60e7a34740d5b193700ee0b
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-12 19:44:18 +0500

    improve(DropDownList): задел начальных данных и отображение их в компоненте списка в SearchByString

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import Header from '../Header/Header';
-import NavSearch from '../NavSearch/NavSearch';
 import { useEffect, useState } from 'react';
+import initialComments from '../../utils/initialComments';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 function App() {
   // цвет темы подтягивается из настроек ОС и сохраняется в localStorage
   const isNightTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches;
   const defaultTheme = isNightTheme ? 'night' : 'day';
   const [theme, setTheme] = useState(localStorage.getItem('app-theme') || defaultTheme);
-
+  const [initialComment, setInitialComment] = useState(initialComments);
+  
   useEffect(() => {
     localStorage.setItem('app-theme', theme);
     document.documentElement.setAttribute('app-theme', theme);
@@ -20,8 +22,9 @@ function App() {
         theme={theme}
         setTheme={setTheme}
       />
-      <NavSearch 
+      <Main 
         theme={theme}
+        data={initialComment}
       />
     </>
   );
--- src/components/NavSearch/NavSearch.js
+++ src/components/NavSearch/NavSearch.js
@@ -1,12 +1,13 @@
 import './NavSearch.css';
 import SearchByString from '../SearchByString/SearchByString';
 
-function NavSearch({ theme }) {
+function NavSearch({ theme, data }) {
   return (
     <nav className={`nav-search nav-search_${theme}`}>
       <SearchByString 
         placeholder='Name'
         theme={theme}
+        data={data}
       />
     </nav>
   )
