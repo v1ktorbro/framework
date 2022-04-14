de8916b60bc83082b9a412634f5a8bb1449188d5
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-14 18:01:20 +0500

    fix(SearchByString): корректная работа компонента. стили в соответствии с выбранной темой

--- src/components/SearchByString/SearchByString.js
+++ src/components/SearchByString/SearchByString.js
@@ -1,32 +1,81 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './SearchByString.css';
 import BtnResetCross from '../BtnResetCross/BtnResetCross';
 import DropDownList from '../DropDownList/DropDownList';
 
 function SearchByString({ placeholder, theme, data }) {
-  const [searchString, setSearchString] = useState('');
+  const [stringValue, setStringValue] = useState('');
+  const [isFocusInput, setIsFocusInput] = useState(false);
+  const [isOpenListSearchedResult, setIsOpenListSearchedResult] = useState(false);
+  //const [dataSearchedResult, setDataSearchedResult] = useState([]);
+
+  //const containerOfInput = (evt) => {return evt.target.closest('.search-by-string__container')};
+
+  const borderStyleForInput = () => {
+    const containerOfInput = document.querySelector('.search-by-string__container');
+    if (isOpenListSearchedResult) {
+      containerOfInput.style.borderBottom = 'none';
+      containerOfInput.style.borderBottomLeftRadius = '0px';
+      containerOfInput.style.borderBottomRightRadius = '0px';
+    } else if (!isOpenListSearchedResult && isFocusInput) {
+      containerOfInput.style.borderBottom = (theme === 'naight' ? '1px solid #fff' : '1px solid #000');
+      containerOfInput.style.borderBottomLeftRadius = '8px';
+      containerOfInput.style.borderBottomRightRadius = '8px';
+    }
+    else {
+      containerOfInput.style.borderBottom = (theme === 'naight' ? '1px solid rgba(255, 255, 255, .3)' : '1px solid rgba(0, 0, 0, .3)');
+      containerOfInput.style.borderBottomLeftRadius = '8px';
+      containerOfInput.style.borderBottomRightRadius = '8px';
+    }
+  };
+
+  const styleOnFocusContainerInput = () => {
+      setIsFocusInput(true);
+      const containerOfInput = document.querySelector('.search-by-string__container');
+      containerOfInput.style.borderBottom = (theme === 'naight' ? '1px solid #fff' : '1px solid #000');
+      console.log('фокус есть', isFocusInput);
+  };
+
+  const styleOnBlurContainerInput = () => {
+    setIsFocusInput(false);
+    const containerOfInput = document.querySelector('.search-by-string__container');
+    containerOfInput.style.borderBottom = (theme === 'naight' ? '1px solid rgba(255, 255, 255, .3)' : '1px solid rgba(0, 0 , 0, .3)');
+    console.log('фокуcа нет', isFocusInput);
+  };
 
   const onChangeSearch = (evt) => {
     const {value} = evt.target;
-    setSearchString(value);
+    setStringValue(value);
+    value.length ? setIsOpenListSearchedResult(true) : setIsOpenListSearchedResult(false);
   }
 
   function handleResetButton(){
-    setSearchString('');
+    setStringValue('');
+    setIsOpenListSearchedResult(false);
   }
+
+  useEffect(() => {
+    borderStyleForInput();
+  }, [isOpenListSearchedResult, theme]);
 
   return (
     <>
-      <div className={`search-by-string search-by-string_${theme}`}>
-        <div className='search-by-string__container'>
+      <div 
+        className={`search-by-string search-by-string_${theme}`}
+      >
+        <div 
+          className={`search-by-string__container search-by-string__container_${theme}`}
+          onFocus={styleOnFocusContainerInput}
+          onBlur={styleOnBlurContainerInput}
+        >
           <input 
             className={`search-by-string__input search-by-string__input_${theme}`}
             type='text'
-            value={searchString}
+            value={stringValue}
             onChange={onChangeSearch}
             placeholder={placeholder}
           />
-          { searchString.length > 0 &&
+          { stringValue.length > 0 &&
             <BtnResetCross 
               hStyle={{right: '18px'}}
               handleReset={handleResetButton}
@@ -37,6 +86,8 @@ function SearchByString({ placeholder, theme, data }) {
         <DropDownList
           theme={theme}
           data={data}
+          isOpen={isOpenListSearchedResult}
+          setIsOpen={setIsOpenListSearchedResult}
         />
       </div>
     </>
