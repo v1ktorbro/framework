f80b3cb31b8238ce0fe82849c2d612db5f2214c4
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-14 17:57:32 +0500

    fix(DropDownList): стили компонента в соттветствии с выбранной темы

--- src/components/DropDownList/DropDownList.js
+++ src/components/DropDownList/DropDownList.js
@@ -1,15 +1,19 @@
 import './DropDownList.css';
 
-function DropDownList({ theme, data }) {
+function DropDownList({ theme, data, isOpen }) {
   return (
-    <ul className={`drop-down-list drop-down-list_${theme}`}>
+    <ul 
+      className={`drop-down-list drop-down-list_${theme} ${isOpen ? 'drop-down-list_open' : 'drop-down-list_close'}`}
+    >
     {data.map((comment, index) => {
       return (
         <li 
           key={index} 
           className='drop-down-list__item'
           >
-            <span className='drop-down-list__text-item'>{comment.author}</span>
+            <span 
+              className='drop-down-list__text-item'
+            >{comment.author}</span>
         </li>
       );
     })}
