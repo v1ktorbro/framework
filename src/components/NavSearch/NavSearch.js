728f1d9a1265706cf9fd7eaa39ba6f6b2d140702
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-28 18:14:21 +0500

    refactor(NavSearch): throw handler from App in children component

--- src/components/NavSearch/NavSearch.js
+++ src/components/NavSearch/NavSearch.js
@@ -2,18 +2,22 @@ import './NavSearch.css';
 import SearchByString from '../SearchByString/SearchByString';
 import FilterSelectListItem from '../FilterSelectListItem/FilterSelectListItem';
 
-function NavSearch({ theme, data, handleNameInputSearch }) {
+function NavSearch({ theme, data, searchData, handlerInputSearchNamePicture, handlerSelectListNameAuthor, handlerResetListNameAuthor }) {
   return (
     <section className={`section-search section-search_${theme}`}>
       <SearchByString 
         placeholder='Name'
         theme={theme}
         data={data}
-        handleNameInputSearch={handleNameInputSearch}
+        handlerInputSearchNamePicture={handlerInputSearchNamePicture}
       />
       <FilterSelectListItem 
         theme={theme}
         data={data}
+        placeholder='Author'
+        handlerSelectListNameAuthor={handlerSelectListNameAuthor}
+        searchData={searchData}
+        handlerResetListNameAuthor={handlerResetListNameAuthor}
       />
     </section>
   )
