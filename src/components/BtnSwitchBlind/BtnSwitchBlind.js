75523935fffc9af01509794a66040e2c30d2c35d
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-28 18:05:01 +0500

    refactor(BtnSwitchBlind): managing a component from a parent

--- src/components/BtnSwitchBlind/BtnSwitchBlind.js
+++ src/components/BtnSwitchBlind/BtnSwitchBlind.js
@@ -1,18 +1,11 @@
 import React from "react";
 import './BtnSwitchBlind.css';
 
-function BtnSwitchBlind ({theme, onClick}) {
-  const [isOpen, setIsOpen] = React.useState(false);
-
-  const handleClick = (evt) => {
-    setIsOpen(prev => !prev);
-    onClick(evt);
-  };
-
+function BtnSwitchBlind ({theme, onClick, isOpen}) {
   return (
     <button 
       type='button' 
-      onClick={(evt) => handleClick(evt)} 
+      onClick={onClick} 
       className={`btn-switch-blind btn-switch-blind_${theme} ${isOpen ? 'btn-switch-blind_open' : 'btn-switch-blind_close'}`} 
     />
   );
