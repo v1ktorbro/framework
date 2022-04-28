0f20be50696325339cf437fce7159577381a3120
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-04-28 14:26:28 +0500

    improve(BtnSwitchBlind): add props onClick  and take him from high component

--- src/components/BtnSwitchBlind/BtnSwitchBlind.js
+++ src/components/BtnSwitchBlind/BtnSwitchBlind.js
@@ -1,17 +1,18 @@
 import React from "react";
 import './BtnSwitchBlind.css';
 
-function BtnSwitchBlind ({theme}) {
+function BtnSwitchBlind ({theme, onClick}) {
   const [isOpen, setIsOpen] = React.useState(false);
 
-  const onClick = () => {
+  const handleClick = (evt) => {
     setIsOpen(prev => !prev);
+    onClick(evt);
   };
 
   return (
     <button 
       type='button' 
-      onClick={onClick} 
+      onClick={(evt) => handleClick(evt)} 
       className={`btn-switch-blind btn-switch-blind_${theme} ${isOpen ? 'btn-switch-blind_open' : 'btn-switch-blind_close'}`} 
     />
   );
