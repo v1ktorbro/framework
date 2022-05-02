a4118fb62222ae7b4b6355fc8f3d08490fcfceea
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-05-02 19:33:23 +0500

    feat(FilterSelectTimeInterval): add of inputs handlers error

--- src/components/FilterSelectTimeInterval/FilterSelectTimeInterval.js
+++ src/components/FilterSelectTimeInterval/FilterSelectTimeInterval.js
@@ -3,11 +3,14 @@ import React from 'react';
 import BtnResetCross from '../BtnResetCross/BtnResetCross';
 import BtnSwitchBlind from '../BtnSwitchBlind/BtnSwitchBlind';
 import { borderStyleHandlerThemeForFilter } from '../../utils/utils';
+import { useInput } from '../FormValidator/FormValidator';
 
 function FilterSelectTimeInterval({theme, nameFilter}) {
   const [inputsValue, setInputsValue] = React.useState({from: '', before: ''});
-  const [isOpenTimeInterval, setIsOpenTimeInterval] = React.useState(false);
+  const [isOpenTimeInterval, setIsOpenTimeInterval] = React.useState(true);
   const [isFocus, setIsFocus] = React.useState(false);
+  const inputTimeFromValidator = useInput('', {isEmpty: true, minLength: 4, maxLength: 4});
+  const inputTimeBeforeValidator = useInput('', {isEmpty: true, minLength: 4, maxLength: 4});
 
   const toggleOpenTimeInterval = () => {
     setIsOpenTimeInterval(prev => !prev);
@@ -18,8 +21,10 @@ function FilterSelectTimeInterval({theme, nameFilter}) {
     const {id} = evt.target;
     const {value} = evt.target;
     if (id === 'from') {
+      inputTimeFromValidator.onChange(evt);
       setInputsValue({...inputsValue, from: value});
     } else {
+      inputTimeBeforeValidator.onChange(evt);
       setInputsValue({...inputsValue, before: value});
     }
   };
@@ -87,23 +92,41 @@ function FilterSelectTimeInterval({theme, nameFilter}) {
       </div>
       <div className={`filter-select-time-interval__form-data filter-select-time-interval__form-data_${theme}  ${isOpenTimeInterval && 'filter-select-time-interval__form-data_open'}`}>
         <div className='filter-select-time-interval__form-data-container'>
-          <input 
-            className={`filter-select-time-interval__input-date filter-select-time-interval__input-date_${theme} filter-select-time-interval__input-date_from`}
-            onChange={handlerValueInputs}
-            placeholder='from'
-            id='from'
-            value={inputsValue.from}
-            type='number'
-          />
-          <span className='filter-select-time-interval__dash-sigh'>&mdash;</span>
-          <input 
-            className={`filter-select-time-interval__input-date filter-select-time-interval__input-date_${theme} filter-select-time-interval__input-date_before`}
-            onChange={handlerValueInputs}
-            placeholder='before'  
-            id='before'
-            value={inputsValue.before}
-            type='number'
+          <div className='filter-select-time-interval__input-date-wrapper'>
+            <input 
+              className={`filter-select-time-interval__input-date filter-select-time-interval__input-date_${theme} filter-select-time-interval__input-date_from`}
+              onChange={handlerValueInputs}
+              placeholder='from'
+              id='from'
+              value={inputsValue.from}
+              type='number'
+              onFocus={inputTimeFromValidator.onFocus}
+              onBlur={inputTimeFromValidator.onBlur}
             />
+            <ul className='filter-select-time-interval__error-list'>
+              {(inputTimeFromValidator.isBlur && inputTimeFromValidator.isEmpty) && <li className={`filter-select-time-interval__item-list`}>Поле не может быть пустым</li>}
+              {(inputTimeFromValidator.isFocus && inputTimeFromValidator.minLengthError) && <li className={`filter-select-time-interval__item-list`}>Минимальная длина 4 символа</li>}
+              {(inputTimeFromValidator.maxLengthError) && <li className={`filter-select-time-interval__item-list`}>Максимальная длина 4 символа</li>}
+            </ul>
+          </div>
+          <span className='filter-select-time-interval__dash-sigh'>&mdash;</span>
+          <div className='filter-select-time-interval__input-date-wrapper'>
+            <input 
+              className={`filter-select-time-interval__input-date filter-select-time-interval__input-date_${theme} filter-select-time-interval__input-date_before`}
+              onChange={handlerValueInputs}
+              placeholder='before'  
+              id='before'
+              value={inputsValue.before}
+              type='number'
+              onFocus={inputTimeBeforeValidator.onFocus}
+              onBlur={inputTimeBeforeValidator.onBlur}
+            />
+            <ul className='filter-select-time-interval__error-list'>
+              {(inputTimeBeforeValidator.isBlur && inputTimeBeforeValidator.isEmpty) && <li className={`filter-select-time-interval__item-list`}>Поле не может быть пустым</li>}
+              {(inputTimeBeforeValidator.isFocus && inputTimeBeforeValidator.minLengthError) && <li className={`filter-select-time-interval__item-list`}>Минимальная длина 4 символа</li>}
+              {(inputTimeBeforeValidator.maxLengthError) && <li className={`filter-select-time-interval__item-list`}>Максимальная длина 4 символа</li>}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
