adf4e5c6625ae6d267561201465587aa9d6236b8
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-05-04 10:05:01 +0500

    refactor(Main): throw common function from App in children component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,14 @@
 import './Main.css';
 import NavSearch from '../NavSearch/NavSearch';
 
-function Main({ theme, data, handlerSelectNamePicture, 
-  handlerSelectListNameAuthor, handlerSelectListLocation, handlerSearchTimeInterval }) {
+function Main({ theme, data, handlerSetValueParamSearch }) {
   return (
     <>
       <main className='main'>
         <NavSearch 
           theme={theme}
           data={data}
-          handlerSelectNamePicture={handlerSelectNamePicture}
-          handlerSelectListNameAuthor={handlerSelectListNameAuthor}
-          handlerSelectListLocation={handlerSelectListLocation}
-          handlerSearchTimeInterval={handlerSearchTimeInterval}
+          handlerSetValueParamSearch={handlerSetValueParamSearch}
         />
       </main>
     </>
