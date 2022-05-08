fab7293bd6f0fc9eb4fc8b5ed61fadeea9bbafea
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-05-08 19:31:40 +0500

    fix(CardImage): add data about author and location

--- src/components/CardImage/CardImage.js
+++ src/components/CardImage/CardImage.js
@@ -1,8 +1,10 @@
 import './CardImage.css';
 import React from 'react';
+import api from '../../utils/Api';
 
-function CardImage({dataCard}) {
+function CardImage({ dataCard, authorOfCard, locationOfCard}) {
   const [isHoverCard, setIsHoverCard] = React.useState(false);
+  const url = api.url;
 
   const handlerHoverState = () => {
     setIsHoverCard(prev => !prev);
@@ -10,19 +12,19 @@ function CardImage({dataCard}) {
 
   return (
     <article className='card-image' onMouseEnter={handlerHoverState} onMouseLeave={handlerHoverState} >
-      <img alt='something image' className='card-image__image' src={dataCard.src} />
+      <img alt={`Picture: ${dataCard.name}`} className='card-image__image' src={`${url}${dataCard.imageUrl}`} />
       <div className='card-image__title-container'>
-        <h2 className='card-image__title'>{dataCard.title}</h2>
+        <h2 className='card-image__title'>{dataCard.name}</h2>
         { isHoverCard &&
           <>
             <h3 className='card-image__subtitle'>Author: 
-              <span className='card-image__subtitle-content'>Rembrandt</span>
+              <span className='card-image__subtitle-content'>{authorOfCard}</span>
             </h3>
             <h3 className='card-image__subtitle'>Created: 
-              <span className='card-image__subtitle-content'>1642</span>
+              <span className='card-image__subtitle-content'>{dataCard.created}</span>
             </h3>
             <h3 className='card-image__subtitle'>Location: 
-              <span className='card-image__subtitle-content'>The Rijksmuseum</span>
+              <span className='card-image__subtitle-content'>{locationOfCard}</span>
             </h3>
           </>
         }
