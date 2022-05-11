bf070634874da6c3ad01b5e99c0115d0e009a221
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-05-11 23:56:24 +0500

    feat(App): create request search by name to server and function handler result

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ import api from '../../utils/Api';
 function App() {
   // по умолчанию, цвет темы подтягивается из настроек ОС и сохраняется в localStorage
   const [theme, setTheme] = React.useState(localStorage.getItem('app-theme') || defaultTheme);
+  const [db, setDb] = React.useState([]);
   const [listPaintings, setListPaintings] = React.useState([]);
   const [listAuthors, setListAuthors] = React.useState([]);
   const [listLocations, setListLocations] = React.useState([]);
@@ -23,14 +24,55 @@ function App() {
   };
 
   const getDataFromApi = () => {
-    Promise.all([api.getListPaintings(), api.getListAuthors(), api.getListLocations()]).then(([listPaintingsFromApi, listAuthorsFromApi, listLocationsFromApi]) => {
-      setListPaintings(listPaintingsFromApi);
-      setListAuthors(listAuthorsFromApi);
-      setListLocations(listLocationsFromApi);
+    api.getAllData().then((res) => {
+      setDb(res);
+      setListPaintings(res.paintings);
+      setListAuthors(res.authors);
+      setListLocations(res.locations);
     }).catch((err) => {
       return console.log('Ошибка при получении данных с сервера:', err);
     });
   };
+
+  const searchByNamePicture = (namePicture) => {
+    console.log(namePicture);
+    if (namePicture.length) {
+      api.searchPictureByName(namePicture).then((res) => {
+          setListAuthors(filterNewArrFromApi(res, 'authorId', listAuthors));
+          setListLocations(filterNewArrFromApi(res, 'locationId', listLocations));
+      }).catch((err) => {
+        return console.log(`Ошибка при получении данных поиска картики:`, err);
+      });
+    } else {
+      setListAuthors(db.authors);
+      setListLocations(db.locations);
+    }
+  };
+
+  const filterNewArrFromApi = (arrFromApi, keyNameId, listArrData) => {
+    let arrUniqueId = [];
+    let newArr = [];
+    const obj = {};
+    for (let i = 0; i < arrFromApi.length; i++) {
+      const currentEl = arrFromApi[i][keyNameId];
+      if (!(currentEl in obj)) {
+        obj[currentEl] = 1;
+      } else {
+        obj[currentEl] += 1;
+      }
+    }
+    const keys = Object.keys(obj);
+    keys.forEach((key) => arrUniqueId.push(key));
+    arrUniqueId.forEach((id) => {
+      console.log(id);
+      newArr.push(listArrData[id - 1])
+    });
+    return newArr;
+  };
+
+  React.useEffect(() => {
+    Object.keys(db).length && searchByNamePicture(searchData.name);
+  }, [searchData.name]);
 
   React.useEffect(() => {
     getDataFromApi();
