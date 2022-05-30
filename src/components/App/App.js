103059dd1ecf8753798cb875e836aab4ccbf2e16
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-05-31 00:19:59 +0500

    refactor(App): remove handlerSearch logic from App and rewrite in HandlerSearch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,12 +20,9 @@ function App() {
     created: {from: '', before: ''},
   });
   const [reqParamSearch, setReqParamSearch] = React.useState([]);
-  //searchController(searchData, reqParamSearch, db);
-  const useSearch = searchController(searchData, reqParamSearch, db, test1);
-
-  function test1(list) {
-    console.log('это test1', list);
-  }
+  //при любом изменении значении полей данные кидаются в HandlerSearch
+  //получение callBack с новым массивом происходит в getUpdatedListData
+  searchController(searchData, reqParamSearch, db, getUpdatedListData);
   
   const handlerSetValueParamSearch = (keyName, value) => {
     setSearchData({...searchData, [keyName]: value});
@@ -36,8 +33,6 @@ function App() {
     if (keyName != 'created') {
       value.length ? setReqParamSearch([...reqParamSearch, keyName]) : setReqParamSearch(reqParamSearch.filter((item) => item != keyName));
     }
-    //useSearch.setter(setListPaintings)
-    //console.log(useSearch.test(), 'cheeeeck');
   };
 
   const getInitialData = () => {
@@ -55,122 +50,19 @@ function App() {
     setListLocations(initialDataBase.locations);
   };
 
-  const searchByOneParametr = (valueField) => {
-    const newList = (arrList, reqParamSearch) => arrList.filter((itemList) => itemList[reqParamSearch] == searchData[reqParamSearch]);
-    const newListPaintings = newList(db.paintings, valueField);
-    switch (valueField) {
-      case 'name':
-        setListPaintings(newListPaintings);
-        setListAuthors(filterNewArrFromApi(newListPaintings, 'authorId', db.authors));
-        setListLocations(filterNewArrFromApi(newListPaintings, 'locationId', db.locations));
+  function getUpdatedListData(newListArr, nameList) {
+    switch (nameList) {
+      case 'listPaintings':
+        setListPaintings(newListArr);
         break;
-      case 'authorId':
-        setListPaintings(newListPaintings);
-        setListLocations(filterNewArrFromApi(newListPaintings, 'locationId', db.locations));
-        setListAuthors(db.authors);
+      case 'listAuthors':
+        setListAuthors(newListArr);
         break;
-      case 'locationId':
-        setListPaintings(newListPaintings);
-        setListAuthors(filterNewArrFromApi(newListPaintings, 'authorId', db.authors));
-        setListLocations(db.locations);
+      case 'listLocations':
+        setListLocations(newListArr);
         break;
     }
-  };
-
-
-  const handlerValueSearchData = () => {
-    if (!reqParamSearch.length) {
-      Object.keys(db).length && setInitialData(db);
-    } else if (reqParamSearch.length == 1) {
-        searchByOneParametr(reqParamSearch[0]);
-    } else if (reqParamSearch.length == 2) {
-        reqParamSearch.reduce((prevValue, currentValue) => {
-          searchByTwoParameters(prevValue, currentValue);
-        });
-    } else if (reqParamSearch.length == 3) {
-      reqParamSearch.reduce((prevValue, currentValue) => {
-        if (currentValue == reqParamSearch[reqParamSearch.length - 1]) {
-          prevValue = reqParamSearch[reqParamSearch.length - 2];
-          searchByTwoParameters(prevValue, currentValue);
-        }
-      });
-    }
-  };
-
-  const searchByTwoParameters = (firstValueField, secondValueField) => {
-    const newList = (arrList, reqParamSearch) => arrList.filter((itemList) => itemList[reqParamSearch] == searchData[reqParamSearch]);
-    const newListPaintings = newList(listPaintings, secondValueField);
-    const newListLocations = filterNewArrFromApi(newListPaintings, 'locationId', db.locations);
-    const newListAuthors = filterNewArrFromApi(newListPaintings, 'authorId', db.authors);
-    switch (firstValueField) {
-      case 'name':
-        if (secondValueField == 'authorId') {
-          setListPaintings(newListPaintings);
-          setListLocations(newListLocations);
-        }
-        if (secondValueField == 'locationId') {
-          setListPaintings(newListPaintings);
-          setListLocations(newListLocations);
-          setListAuthors(newListAuthors);
-        }
-        break;
-      case 'authorId':
-        if (secondValueField == 'name') {
-          setListPaintings(newListPaintings);
-          setListAuthors(newListAuthors);
-          setListLocations(newListLocations);
-        }
-        if (secondValueField == 'locationId') {
-          const newListAuthors = filterNewArrFromApi(newList(db.paintings, firstValueField), 'authorId', db.authors);
-          setListPaintings(newListPaintings);
-          setListAuthors(newListAuthors);
-        }
-        break;
-      case 'locationId':
-        if (secondValueField == 'name') {
-          setListPaintings(newListPaintings);
-          setListAuthors(newListAuthors);
-          setListLocations(newListLocations);
-        }
-        if (secondValueField == 'authorId') {
-          const newListAuthors = filterNewArrFromApi(newList(db.paintings, firstValueField), 'authorId', db.authors);
-          const newListLocation = filterNewArrFromApi(newList(db.paintings, secondValueField), 'locationId', db.locations);
-          setListPaintings(newListPaintings);
-          setListAuthors(newListAuthors);
-          setListLocations(newListLocation);
-        }
-        break;
-    }
-  };
-
-  const filterNewArrFromApi = (arrFromApi, keyNameId, listArrData) => {
-    let arrUniqueId = [];
-    let newArr = [];
-    const obj = {};
-    for (let i = 0; i < arrFromApi.length; i++) {
-      const currentEl = arrFromApi[i][keyNameId];
-      if (!(currentEl in obj)) {
-        obj[currentEl] = 1;
-      } else {
-        obj[currentEl] += 1;
-      }
-    }
-    const keys = Object.keys(obj);
-    keys.forEach((key) => arrUniqueId.push(key));
-    arrUniqueId.forEach((id) => newArr.push(listArrData[id - 1]));
-    return newArr;
-  };
-
-  React.useEffect(() => {
-    //console.log(useSearch);
-    //useSearch.handlerValueSearchData(reqParamSearch);
-    const someFunc = (list) => {
-      console.log('проверяю тут', list);
-    };
-    console.log('кешкеш', useSearch.listPaintings);
-    //useSearch.onChange(someFunc);
-    //console.log('искомое тут', useSearch.listPaintings);
-  }, [reqParamSearch]);
+  }
 
   React.useEffect(() => {
     localStorage.setItem('app-theme', theme);
