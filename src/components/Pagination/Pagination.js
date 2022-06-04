2561952cfe005239bae2cc378d4073414117d2e9
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-04 13:48:33 +0500

    refactor(Pagination): the state of the elements by design

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -6,13 +6,13 @@ function Pagination() {
   const theme = React.useContext(CurrentThemeContext);
 
   return (
-    <ul className={`pagination pagination_theme-${theme}`}>
-    <button type='button' className={`pagination__btn pagination__btn-duble-arrow pagination__btn-duble-arrow_theme_${theme} pagination__btn-duble-arrow_prev`} />
-    <button type='button' className={`pagination__btn pagination__btn-single-arrow pagination__btn-single-arrow_theme_${theme} pagination__btn-single-arrow_prev`} />
+    <ul className={`pagination pagination_theme_${theme}`}>
+    <button type='button' disabled className={`pagination__btn pagination__btn-duble-arrow pagination__btn-duble-arrow_theme_${theme} pagination__btn-duble-arrow_prev`} />
+    <button type='button' disabled className={`pagination__btn pagination__btn-single-arrow pagination__btn-single-arrow_theme_${theme} pagination__btn-single-arrow_prev`} />
       {[...Array(3)].map((elem, index) => {
         return (
           <li 
-            className='pagination__item pagination__item_active'
+            className={`pagination__item ${index == 1 && `pagination__item_active_theme_${theme}`}`}
             key={index}>
             <a className='pagination__text-item'>{index}</a>
           </li>
