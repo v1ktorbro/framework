6f8092961d759dc1c84933d7d572ef3d68140d84
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-17 13:14:31 +0500

    refactor(BtnSwitchBlind): get value theme from context

--- src/components/BtnSwitchBlind/BtnSwitchBlind.js
+++ src/components/BtnSwitchBlind/BtnSwitchBlind.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './BtnSwitchBlind.css';
+import { CurrentThemeContext } from '../../context/CurrentThemeContext';
 
-function BtnSwitchBlind ({theme, onClick, isOpen, style}) {
+function BtnSwitchBlind ({ onClick, isOpen, style }) {
+  const theme = React.useContext(CurrentThemeContext);
+  
   return (
     <button 
       type='button' 
       onClick={onClick} 
-      className={`btn-switch-blind btn-switch-blind_${theme} ${isOpen ? 'btn-switch-blind_open' : 'btn-switch-blind_close'}`}
+      className={`btn-switch-blind btn-switch-blind_theme-${theme} ${isOpen ? 'btn-switch-blind_open' : 'btn-switch-blind_close'}`}
       style={style}
     />
   );
