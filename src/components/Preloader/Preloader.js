ea31fb7801634e92634d4f612c5f3302ca709c45
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-18 20:44:18 +0500

    refactor(Preloader): write props value in local state and update each time

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -2,18 +2,23 @@ import './Preloader.css';
 import React, { useContext } from 'react';
 import { CurrentThemeContext } from '../../context/CurrentThemeContext';
 
-function Preloader({ isOpen, isProcessSearch = true, messageCurrentProcessSearch='Loading...' }) {
+function Preloader({ preloaderParam }) {
   const theme = useContext(CurrentThemeContext);
+  const [param, setParam] = React.useState({isLoading: false, messageProcess: ''});
+
+  React.useEffect(() => {
+    setParam(preloaderParam);
+  }, [preloaderParam]);
 
   return (
-    <div className={`preloader ${isOpen ? 'preloader_open' : 'preloader_closed'} preloader_theme-${theme}`} >
-      <div className={isProcessSearch ? `preloader__image-moving-circle preloader__image-moving-circle_theme-${theme}` : `preloader__image-error-result preloader__image-error-result_theme-${theme}`} >
-        <span className={isProcessSearch ? `preloader__image-moving-ball preloader__image-moving-ball_theme-${theme}` : 'preloader__image-error-result' } />
+    <>
+      <div className={`preloader ${param.isLoading ? 'preloader_open' : 'preloader_closed'} preloader_theme-${theme}`} >
+        <div className={`preloader__image-moving-circle preloader__image-moving-circle_theme-${theme}`} >
+          <span className={`preloader__image-moving-ball preloader__image-moving-ball_theme-${theme}`} />
+        </div>
+        <span className={'preloader__description-process'}>{param.messageProcess}</span>
       </div>
-      <span className={isProcessSearch ? 'preloader__description-process' : 'preloader__description-error-result'} > 
-        {messageCurrentProcessSearch}
-      </span>
-    </div>
+    </>
   );
 }
 
