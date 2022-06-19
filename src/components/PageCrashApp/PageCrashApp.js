8be6f5945cb3e6cfb88349afcacb02e979607fc2
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-19 11:38:13 +0500

    refactor(PageCrashApp): write data in the component via the prop

--- src/components/PageCrashApp/PageCrashApp.js
+++ src/components/PageCrashApp/PageCrashApp.js
@@ -2,8 +2,9 @@ import './PageCrashApp.css';
 import React from 'react';
 import { CurrentThemeContext } from '../../context/CurrentThemeContext';
 
-function PageCrashApp() {
+function PageCrashApp({ param }) {
   const theme = React.useContext(CurrentThemeContext);
+
   const [textForComponent] = React.useState({
     title: 'Sorry',
     description: 'The site is down. Please, try again later, we will be back soon.'
@@ -12,8 +13,8 @@ function PageCrashApp() {
   return (
     <section className={`page-crash-app page-crash-app_theme-${theme}`}>
       <div className='page-crash-app__block-text'>
-        <h1 className='page-crash-app__title'>{textForComponent.title}</h1>
-        <p className='page-crash-app__description'>{textForComponent.description}</p>
+        <h1 className='page-crash-app__title'>{param.messageErrorTitle || textForComponent.title}</h1>
+        <p className='page-crash-app__description'>{param.messageErrorDescription || textForComponent.description}</p>
       </div>
     </section>
   );
