568bc8accd021c8f1aac8aa9430789f46834c79f
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-22 15:07:56 +0500

    refactor(PageNotFound): remove btn link

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,16 +1,15 @@
 import './PageNotFound.css';
-import { Link } from 'react-router-dom';
 import React from 'react';
 import { CurrentThemeContext } from '../../context/CurrentThemeContext';
 
 function PageNotFound({param}) {
   const theme = React.useContext(CurrentThemeContext);
+  
   return (
     <section className={`page-not-found page-not-found_theme-${theme}`}>
       <div className='page-not-found__container'>
         <h2 className='page-not-found__title'>{param.title}</h2>
         <p className='page-not-found__description'>{param.description}</p>
-        <Link to='/' className='page-not-found__btn-main-page'>Main page</Link>
       </div>
       <h1 className='page-not-found__status-code'>{param.statusCode}</h1>
     </section>
